d57c8b62ef907e637434be86daa1608e28cd7eaa
zhangwenxue <1257545592@qq.com>
2024-08-08 14:33:54 +0800

    增加type-c/usb模式切换

--- app/src/main/java/android/boot/common/NavigationAccessibilityService.kt
+++ app/src/main/java/android/boot/common/NavigationAccessibilityService.kt
@@ -9,7 +9,6 @@ import android.os.Build
 import android.provider.Settings
 import android.util.Log
 import android.view.Gravity
-import android.view.KeyEvent
 import android.view.LayoutInflater
 import android.view.MotionEvent
 import android.view.View
@@ -22,14 +21,23 @@ import android.view.WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHO
 import android.view.WindowManager.LayoutParams.TYPE_ACCESSIBILITY_OVERLAY
 import android.view.WindowManager.LayoutParams.WRAP_CONTENT
 import android.view.accessibility.AccessibilityEvent
+import androidx.core.view.isVisible
+import java.io.File
+import java.util.Timer
+import java.util.TimerTask
 import java.util.concurrent.Executors
 
 @SuppressLint("InflateParams")
 class NavigationAccessibilityService : AccessibilityService() {
+    private val usbFile = if (Build.MODEL == "C10X")
+        "/sys/devices/platform/yj_micro_usb/yj_micro_usb" else "/sys/devices/platform/hardware_ports_manager/usb_interfaces_sel"
 
     private val executor by lazy {
         Executors.newCachedThreadPool()
     }
+
+    private var usbMode = -1
+
     private val view by lazy {
         val view = LayoutInflater.from(this)
             .inflate(android.boot.common.tools.R.layout.global_menu, null, false) as ViewGroup
@@ -56,18 +64,10 @@ class NavigationAccessibilityService : AccessibilityService() {
                 }
             }
         }
-        view.findViewById<View>(android.boot.common.tools.R.id.back_btn).setOnClickListener {
-            /*val inst = Instrumentation()
-            inst.sendKeyDownUpSync(KeyEvent.KEYCODE_BACK)*/
+        view.findViewById<View>(android.boot.common.tools.R.id.usb_switch).setOnClickListener {
             executor.execute {
                 runCatching {
-                    val process = Runtime.getRuntime().exec(
-                        "input keyevent ${KeyEvent.KEYCODE_BACK}"
-                    )
-
-                    process.errorStream.readBytes().run {
-                        Log.i("_NavigationAccessibilityService", "error: ${this.decodeToString()}")
-                    }
+                    File(usbFile).writeText(if (usbMode == 0) "1" else "0")
                 }.onFailure { it.printStackTrace() }
             }
 
@@ -147,9 +147,61 @@ class NavigationAccessibilityService : AccessibilityService() {
     override fun onServiceConnected() {
         super.onServiceConnected()
         initView()
+        startTimer()
     }
 
     override fun onAccessibilityEvent(event: AccessibilityEvent?) {}
     override fun onInterrupt() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            stopTimer()
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            stopTimer()
+        }
+    }
+
+
+    private fun updateView() {
+        executor.execute {
+            usbMode = runCatching {
+                val text = File(usbFile).readText(Charsets.UTF_8)
+                Log.i("_usb", "usbMode: $text")
+                text.replace(" ","").replace("\n","")
+            }.onFailure {
+                it.printStackTrace()
+            }.getOrElse { "-1" }.toIntOrNull() ?: -1
+            view.post {
+                view.findViewById<View>(android.boot.common.tools.R.id.usb_switch).apply {
+                    isVisible = usbMode != -1
+                    setBackgroundResource(if (usbMode == 0) android.boot.common.tools.R.mipmap.ic_typec else android.boot.common.tools.R.mipmap.ic_usb)
+                }
+            }
+        }
+    }
+
+
+    private var timerTask: TimerTask? = null
+    private val timer = Timer()
+    private fun startTimer() {
+        runCatching {
+            stopTimer()
+            timerTask = object : TimerTask() {
+                override fun run() {
+                    updateView()
+                }
+            }
+            timer.schedule(timerTask, 0, 3000)
+        }
+    }
+
+    private fun stopTimer() {
+        runCatching {
+            timerTask?.cancel()
+            timerTask = null
+        }
     }
 }
