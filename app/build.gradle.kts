5513029c59ca80e41efe00b26e129eefee8c464e
zhangwenxue <1257545592@qq.com>
2024-08-09 09:59:37 +0800

    feat：modify app name

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,7 +19,9 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
+            isShrinkResources = true
+            isDebuggable = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
